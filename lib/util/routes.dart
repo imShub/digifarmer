c0a6102238ec1167eeaa6b9f73879e7f1304e0de
KaranPatra <114423203+KaranPatra@users.noreply.github.com>
2023-09-23 18:24:25 +0530

    Delete lib directory

--- lib/util/routes.dart
+++
@@ -1,7 +0,0 @@
-class MyRoutes {
-  static String welcomeRoute = "/welcome";
-  static String loginRoute = "/login";
-  static String homeRoute = "/home";
-  static String homeDetailsRoute = "/detail";
-  static String cartRoute = "/cart";
-}
